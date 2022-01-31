f4f9c8585e75c91a559bb30315e9075f4523faf0
MohanramAnbukkarasu <44049406+MohanramAnbukkarasu@users.noreply.github.com>
2022-01-31 20:42:05 +0530

    Add files via upload

---
+++ SchedulerDemo/Form1.cs
@@ -0,0 +1,31 @@
+﻿using Syncfusion.Windows.Forms.Schedule;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchedulerDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += OnLoad;
+        }
+
+        private void OnLoad(object sender, EventArgs e)
+        {
+            SimpleScheduleDataProvider data = new SimpleScheduleDataProvider();
+            data.MasterList = new SimpleScheduleAppointmentList();
+            data.FileName = "default.schedule";
+            this.scheduleControl1.ScheduleType = ScheduleViewType.Month;
+            this.scheduleControl1.DataSource = data;
+        }
+    }
+}
